9ce100d4d53e433543407a80cb41d6c038ecc791
Alex Jbanca <alexjb@status.im>
2026-07-28 18:06:48 +0300

    fix(share): drop a duplicated app-foregrounded emitter definition

--- ui/StatusQ/src/externc.cpp
+++ ui/StatusQ/src/externc.cpp
@@ -155,10 +155,6 @@ Q_DECL_EXPORT void statusq_urlscheme_emit_appforegrounded(void* obj) {
     static_cast<Status::UrlSchemeEvent*>(obj)->emitAppForegroundedToQt();
 }
 
-Q_DECL_EXPORT void statusq_urlscheme_emit_appforegrounded(void* obj) {
-    static_cast<Status::UrlSchemeEvent*>(obj)->emitAppForegroundedToQt();
-}
-
 Q_DECL_EXPORT void statusq_urlscheme_delete(void* obj) {
     static_cast<QObject*>(obj)->deleteLater();
 }
